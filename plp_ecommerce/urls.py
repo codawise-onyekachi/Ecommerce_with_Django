05e6050b115666fd79312c32d97b725cb3e2d402
codawise-onyekachi <onyekachihopeuzoma@gmail.com>
2024-06-21 20:29:08 +0100

    updated files

--- plp_ecommerce/urls.py
+++ plp_ecommerce/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'plp_ecommerce'
+
 urlpatterns = [
     path('', views.product_list, name='product_list'),
+    path('<int:pk>/', views.product_details, name='product_details'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>', views.customer_detail, name='customer_detail'),
+    
+    
 
 ]
--- plp_ecommerce/views.py
+++ plp_ecommerce/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import get_object_or_404, render
+from .models import Product, Customer
 #from django.http import HttpResponse
 #from django.shortcuts import HttpResponse
 
@@ -7,6 +7,27 @@ from .models import Product
 def product_list(request):
     products = Product.objects.all()
     context = {
-        'products': products,
+        'products': products
     }
     return render(request, 'plp_ecommerce/product_list.html', context)
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'plp_ecommerce/product_details.html', context)
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    context = {
+        'customers' : customers
+    }
+    return render(request, 'plp_ecommerce/customer_list.html', context)
+
+def customer_detail(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    context = {
+        'customer': customer
+    }
+    return render(request, 'plp_ecommerce/customer_detail.html', context)
